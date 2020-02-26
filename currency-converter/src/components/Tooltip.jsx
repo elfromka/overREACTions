9086e3e1c380d87a0681faf5be2ceb287b1a8f7d
Sebestyen Orsi <elfromka@gmail.com>
2020-02-26 22:39:52 +0200

    Small fix in Tooltip component

--- currency-converter/src/components/Tooltip.jsx
+++ currency-converter/src/components/Tooltip.jsx
@@ -3,5 +3,5 @@ import React from "react";
 export default function Tooltip(props) {
     const { message } = props;
 
-    return <span class="tooltiptext">{message}</span>;
+    return <span className="tooltiptext">{message}</span>;
 }
