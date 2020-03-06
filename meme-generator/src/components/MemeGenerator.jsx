448ec503b1d1b43e820c46730102218b16ccb182
Sebestyen Orsi <elfromka@gmail.com>
2020-03-06 22:23:41 +0200

    setting up states in MemeGenerator component + added API call to get images for the meme

--- meme-generator/src/components/MemeGenerator.jsx
+++ meme-generator/src/components/MemeGenerator.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function MemeGenerator() {
+    const [topText, setTopText] = useState("");
+    const [bottomText, setBottomText] = useState("");
+    const [randomImage, setRandomImage] = useState(
+        "http://i.imgflip.com/1bij.jpg"
+    );
+    const [allMemeImages, setAllMemeImages] = useState([]);
+
+    async function fetchData() {
+        const response = await fetch("https://api.imgflip.com/get_memes");
+        response.json().then(response => {
+            const { memes } = response.data;
+            setAllMemeImages(memes);
+        });
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
         <div>
             <h1>fdjgdkflgjdkfgdkjlf</h1>
