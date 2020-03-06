aab3c20939edc16a9ffd0ee59464fe2f2a5146c2
Sebestyen Orsi <elfromka@gmail.com>
2020-03-06 22:45:03 +0200

    Adding inputs for topText and bottomText + onchange handler as a test

--- meme-generator/src/components/MemeGenerator.jsx
+++ meme-generator/src/components/MemeGenerator.jsx
@@ -20,9 +20,36 @@ export default function MemeGenerator() {
         fetchData();
     }, []);
 
+    const handleChange = e => {
+        e.target.name === "topText"
+            ? setTopText(e.target.value)
+            : setBottomText(e.target.value);
+    };
+
     return (
         <div>
-            <h1>fdjgdkflgjdkfgdkjlf</h1>
+            <form className="meme-form">
+                <input
+                    type="text"
+                    value={topText}
+                    name="topText"
+                    placeholder="Top text"
+                    onChange={handleChange}
+                />
+                <br />
+                <input
+                    type="text"
+                    value={bottomText}
+                    name="bottomText"
+                    placeholder="Bottom text"
+                    onChange={handleChange}
+                />
+                <button>Generate</button>
+            </form>
+            <br />
+            <h1>
+                {topText} {bottomText}
+            </h1>
         </div>
     );
 }
