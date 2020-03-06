a16903833cf0522c68bafd7156e026d83af81d17
Sebestyen Orsi <elfromka@gmail.com>
2020-03-06 22:02:49 +0200

    Added styling + created the header component

---
+++ meme-generator/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header>
+            <img
+                src="http://pngall.com/wp-content/uploads/2016/05/Trollface.png"
+                alt="Meme"
+            />
+            <p>Meme Generator</p>
+        </header>
+    );
+}
---
+++ meme-generator/src/components/MemeGenerator.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MemeGenerator() {
+    return (
+        <div>
+            <h1>fdjgdkflgjdkfgdkjlf</h1>
+        </div>
+    );
+}
