290b485626173f3f51588370e09df36102d88fb8
Sebestyen Orsi <elfromka@gmail.com>
2020-03-08 00:02:00 +0200

    read the files(images) and set them to the state for displaying them in the browser later + installed cuid for generating ids for the uploaded images

--- img-dnd-upload/src/App.js
+++ img-dnd-upload/src/App.js
@@ -1,12 +1,27 @@
-import React, { useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import Dropzone from "./components/Dropzone";
+import cuid from "cuid";
 
 import "./App.css";
 
 function App() {
+    const [images, setImages] = useState([]);
+
     const onDrop = useCallback(acceptedFiles => {
-        console.log(acceptedFiles);
+        acceptedFiles.map(image => {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                setImages(prevState => [
+                    ...prevState,
+                    { id: cuid(), src: e.target.result }
+                ]);
+            };
+            reader.readAsDataURL(image);
+            return image;
+        });
     }, []);
+
+    console.log(images);
 
     return (
         <div className="App">
