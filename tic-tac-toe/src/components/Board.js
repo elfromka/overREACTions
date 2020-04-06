bd7e38840af137d6bd46a2e954fb8c6cabfb4645
Sebestyen Orsi <elfromka@gmail.com>
2020-04-06 21:15:17 +0300

    Styling the components

--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -1,5 +1,28 @@
 import React from "react";
+import Square from "./Square";
 
-const Board = () => <div>Board</div>;
+const style = {
+    border: "4px solid darkblue",
+    borderRadius: "10px",
+    width: "250px",
+    height: "250px",
+    margin: "0 auto",
+    display: "grid",
+    gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
+};
+
+const Board = ({ squares, onClick }) => (
+    <div style={style}>
+        <Square value="1" onClick={() => onClick("test")} />
+        <Square value="2" onClick={() => onClick("test")} />
+        <Square value="3" onClick={() => onClick("test")} />
+        <Square value="4" onClick={() => onClick("test")} />
+        <Square value="5" onClick={() => onClick("test")} />
+        <Square value="6" onClick={() => onClick("test")} />
+        <Square value="7" onClick={() => onClick("test")} />
+        <Square value="8" onClick={() => onClick("test")} />
+        <Square value="9" onClick={() => onClick("test")} />
+    </div>
+);
 
 export default Board;
