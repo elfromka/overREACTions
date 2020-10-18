00f6c376caea814af08df5c33d6308fed797138d
Orsi Sebestyen <elfromka@gmail.com>
2020-10-18 22:20:54 +0300

    Finishing the React Footer

--- react-components/footer/src/components/footer/styles/footer.js
+++ react-components/footer/src/components/footer/styles/footer.js
@@ -5,12 +5,43 @@ export const Container = styled.div`
     background: radial-gradient(circle, rgba(92, 39, 251, 1) 0%, rgba(112, 71, 274, 1) 100%);
 `
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 1000px;
+    margin: 0 auto;
+`
 
-export const Column = styled.div``
+export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    margin-left: 60px;
+`
 
-export const Row = styled.div``
+export const Row = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+    grid-gap: 20px;
+`
 
-export const Link = styled.div``
+export const Link = styled.div`
+    color: #fff;
+    margin-bottom: 20px;
+    font-size: 18px;
+    text-decoration: none;
 
-export const Title = styled.div``
+    &:hover {
+        color: #ff9c00;
+        transition: 200ms ease-in;
+        cursor: pointer;
+    }
+`
+
+export const Title = styled.div`
+    font-size: 24px;
+    color: #fff;
+    margin-bottom: 40px;
+    font-weight: bold;
+`
